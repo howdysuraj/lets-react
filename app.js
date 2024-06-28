559ebb69eaaeaa5f4674c807675d7a8dc2c42e65
howdysuraj <howdysuraj@gmail.com>
2024-06-28 15:48:14 +0530

    added sibling componets

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
     // Your React code can go here
     const heading =   React.createElement('div', {id:'parent'}, 
-        React.createElement('h1',{id:'heading'},'Hello Suraj, from React 18!'));
+        [React.createElement('h1',{id:'heading'},'Hello Suraj, from React 18!'),
+         React.createElement('h1',{id:'heading'},'Hello Suraj, from React 18!')    
+        ]);
     const root = ReactDOM.createRoot(document.getElementById('root'));
     root.render(heading);

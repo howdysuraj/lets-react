71e7f4498583db7c5d5732c774fc69070150b6a0
howdysuraj <howdysuraj@gmail.com>
2024-06-28 15:43:07 +0530

    Added nested elements

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
     // Your React code can go here
-    const heading =   React.createElement('h1', {}, 'Hello Suraj, from React 18!');
+    const heading =   React.createElement('div', {id:'parent'}, 
+        React.createElement('h1',{id:'heading'},'Hello Suraj, from React 18!'));
     const root = ReactDOM.createRoot(document.getElementById('root'));
     root.render(heading);

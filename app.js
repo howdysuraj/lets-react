44ffc149aea0b394d9b2a2e4ffe947525c9c656e
howdysuraj <howdysuraj@gmail.com>
2024-06-28 12:08:10 +0530

    feat: moved script to a new file

---
+++ app.js
@@ -0,0 +1,4 @@
+    // Your React code can go here
+    const heading =   React.createElement('h1', {}, 'Hello Suraj, from React 18!');
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(heading);

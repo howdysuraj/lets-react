e63e3c6da31333577843e34cc94b114a70ec92cc
howdysuraj <howdysuraj@gmail.com>
2024-06-30 19:59:36 +0530

    added parcel

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-    // Your React code can go here
-    const heading =   React.createElement('div', {id:'parent'}, 
-        [React.createElement('h1',{id:'heading'},'Hello Suraj, from React 18!'),
-         React.createElement('h1',{id:'heading'},'Hello Suraj, from React 18!')    
-        ]);
-    const root = ReactDOM.createRoot(document.getElementById('root'));
-    root.render(heading);
+import  React  from "react";
+import  ReactDOM  from "react-dom/client";
+
+// Your React code can go here
+const heading = React.createElement("div", { id: "parent" }, [
+  React.createElement("h1", { id: "heading" }, "Hello Suraj, from React 18! form heaven"),
+  React.createElement("h1", { id: "heading" }, "Hello Suraj, from React 18!"),
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);

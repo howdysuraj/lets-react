021594f00c623906c7a24be2677da597f41c253e
howdysuraj <howdysuraj@gmail.com>
2024-07-01 09:20:34 +0530

    jsx added

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ import  React  from "react";
 import  ReactDOM  from "react-dom/client";
 
 // Your React code can go here
-const heading = React.createElement("div", { id: "parent" }, [
-  React.createElement("h1", { id: "heading" }, "Hello Suraj, from React 18! form heaven"),
-  React.createElement("h1", { id: "heading" }, "Hello Suraj, from React 18!"),
-]);
+const heading = <h1>hello jsx</h1>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);

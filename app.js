98ac1329c8ae92c16898d7675c9bc5880eb21b8b
howdysuraj <howdysuraj@gmail.com>
2024-07-02 11:45:28 +0530

    feat: added functional componet

--- app.js
+++ app.js
@@ -3,5 +3,18 @@ import  ReactDOM  from "react-dom/client";
 
 // Your React code can go here
 const heading = <h1>hello jsx</h1>;
+
+//React Componets 
+
+//Class Based Componets
+
+//Functional Componets
+
+const HeadingComponet = ({name}) => (
+    <div id="container">
+  <h1>Hello {name} from Heading functional componet</h1>
+
+    </div>)
+   
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComponet name="Suraj"></HeadingComponet>);

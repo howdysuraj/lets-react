763a898d51416b18b02183866e1df55cbc082d8d
howdysuraj <howdysuraj@gmail.com>
2024-07-02 11:49:21 +0530

    componet composition

--- app.js
+++ app.js
@@ -8,10 +8,14 @@ const heading = <h1>hello jsx</h1>;
 
 //Class Based Componets
 
+
 //Functional Componets
+const TitleComponent = () => <h1>This is title</h1>
 
 const HeadingComponet = ({name}) => (
     <div id="container">
+         {/* componet composition */}
+        <TitleComponent/>
   <h1>Hello {name} from Heading functional componet</h1>
 
     </div>)
